1c08820fe1700b5869d0eaaf384e92089297cacd
wangxydlut <wangxydlut@81521f60-6532-1445-2e97-40daa67050c5>
2011-03-24 15:47:34 +0000

    Fixed the bugs that Current Status set problems.

--- trunk/ServerHost/Server.cs
+++ trunk/ServerHost/Server.cs
@@ -15,40 +15,42 @@ namespace ServerHost
         public CommunicationState CurrentState
         {
             get { return ManagerHost.State; }
-            private set;
         }
         
-        public void StartServer()
+        public void StartServer(String port)
         {
-            if (ManagerHost == null 
-                || this.CurrentState== CommunicationState.Closed)
-            {            
-                try
-                {
-                    ManagerHost = new ServiceHost(typeof(CRMService));                
-                    ManagerHost.Opened += new EventHandler(ManagerHost_Opened);
-                    ManagerHost.Closed += new EventHandler(ManagerHost_Closed);
-                    IPAddress[] addresslist = Dns.GetHostAddresses(Dns.GetHostName());
-                    NetTcpBinding binding = new NetTcpBinding();
-                    String endpointaddress = "net.tcp://" + addresslist[0].ToString() + ":" + this.txt_port.Text + "/UserService";
-                    ServiceEndpoint endPoint = ManagerHost.AddServiceEndpoint(
-                        typeof(IUserService), binding,
-                        endpointaddress);
-                    String customeraddress = "net.tcp://" + addresslist[0].ToString() + ":" + this.txt_port.Text + "/CustomerService";
-                    ServiceEndpoint customerEndPoint = ManagerHost.AddServiceEndpoint(
-                        typeof(ICustomerService), binding, customeraddress);
-                    String taskaddress = "net.tcp://" + addresslist[0].ToString() + ":" + this.txt_port.Text + "/TaskService";
-                    ServiceEndpoint taskEndPoint = ManagerHost.AddServiceEndpoint(
-                        typeof(ITaskService), binding, taskaddress);
-                    //endPoint.Behaviors.Add();
-                    //OperationContext.Current.Channel.Closed += new EventHandler(Channel_Closed);
-                    ManagerHost.Open();
+            if (ManagerHost == null) 
+                
+            {   
+                if (this.CurrentState== CommunicationState.Closed)
+                {                
+                    try
+                    {
+                        ManagerHost = new ServiceHost(typeof(CRMService));                
+                        ManagerHost.Opened += new EventHandler(ManagerHost_Opened);
+                        ManagerHost.Closed += new EventHandler(ManagerHost_Closed);
+                        IPAddress[] addresslist = Dns.GetHostAddresses(Dns.GetHostName());
+                        NetTcpBinding binding = new NetTcpBinding();
+                        String endpointaddress = "net.tcp://" + addresslist[0].ToString() + ":" + port + "/UserService";
+                        ServiceEndpoint endPoint = ManagerHost.AddServiceEndpoint(
+                            typeof(IUserService), binding,
+                            endpointaddress);
+                        String customeraddress = "net.tcp://" + addresslist[0].ToString() + ":" + port + "/CustomerService";
+                        ServiceEndpoint customerEndPoint = ManagerHost.AddServiceEndpoint(
+                            typeof(ICustomerService), binding, customeraddress);
+                        String taskaddress = "net.tcp://" + addresslist[0].ToString() + ":" + port + "/TaskService";
+                        ServiceEndpoint taskEndPoint = ManagerHost.AddServiceEndpoint(
+                            typeof(ITaskService), binding, taskaddress);
+                        //endPoint.Behaviors.Add();
+                        //OperationContext.Current.Channel.Closed += new EventHandler(Channel_Closed);
+                        ManagerHost.Open();
 
-                }
-                catch (System.Exception ex)
-                {
-                    throw ex;
-                 //this.serverstatuslist.Items.Add(ex.Message.ToString() + Environment.NewLine + ex.StackTrace);
+                    }
+                    catch (System.Exception ex)
+                    {
+                        throw ex;
+                     //this.serverstatuslist.Items.Add(ex.Message.ToString() + Environment.NewLine + ex.StackTrace);
+                    }
                 }
             }
         }
